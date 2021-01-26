c5fadb77395d5bce2122434d9a04c4845441d260
jazewh <77996432+jazewh@users.noreply.github.com>
2021-01-26 13:59:11 -0500

    debut exo

--- exercice.py
+++ exercice.py
@@ -3,21 +3,27 @@
 
 
 import math
-
+pi = math.pi
 def square_root(a: float) -> float:
     return math.sqrt(a)
+    # return a**(1/2) si on ve sous forme de puissance
 
 
 def square(a: float) -> float:
-    return 0.0
+    return a**2
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    s = a+b+c
+    aver = s/3
+    return aver
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    deg = angle_degs*(pi/180)
+    min = angle_mins*(pi/180)
+    sec = angle_secs*(pi/180)
+    return deg,min,sec
 
 
 def to_degrees(angle_rads: float) -> tuple:
@@ -39,7 +45,7 @@ def main() -> None:
 
     print(f"Moyenne des nombres 2, 4, 6: {average(2, 4, 6)}")
 
-    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
+    print(f"Conversion de 180 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
     
     degrees, minutes, seconds = to_degrees(1.0)
     print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
